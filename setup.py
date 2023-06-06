d9ed7848e81e2b4b5084b732f2384e80083c5b73
q9i <46249765+quantum9Innovation@users.noreply.github.com>
2023-06-06 14:23:29 -0700

    Switch to Rye package mananger
    https://github.com/mitsuhiko/rye

--- setup.py
+++ setup.py
@@ -20,8 +20,8 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    package_dir={'': 'package'},
-    packages=setuptools.find_packages(where='package'),
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src'),
     python_requires='>=3.7',
     entry_points={
         'console_scripts': [
